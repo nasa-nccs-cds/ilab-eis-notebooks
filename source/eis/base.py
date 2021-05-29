3f85417ce633c9e49f35c94f4ed21f7e57fb2350
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-05-29 17:15:23 -0400

    Add LIS classes

--- source/eis/base.py
+++ source/eis/base.py
@@ -4,9 +4,18 @@ class EISSingleton:
 
     _instance: "EISSingleton" = None
 
+    def __init__(self, *args, **kwargs ):
+        pass
 
     @classmethod
-    def instance( cls, *args, **kwargs ) -> "EISSingleton":
+    def instance(cls, *args, **kwargs):
         if cls._instance is None:
-            cls._instance =  EISSingleton.__init__( *args, **kwargs )
+            inst = cls(*args, **kwargs)
+            cls._instance = inst
+            cls._instantiated = cls
         return cls._instance
+
+    @classmethod
+    def initialized(cls):
+        """Has an instance been created?"""
+        return hasattr(cls, "_instance") and cls._instance is not None
--- source/eis/smce.py
+++ source/eis/smce.py
@@ -9,12 +9,17 @@ from .base import EISSingleton
 class EIS3(EISSingleton):
 
     def __init__( self, **kwargs ):
+        EISSingleton.__init__( self, **kwargs )
         anon = kwargs.pop( 'anon', False )
         self.s3 = s3fs.S3FileSystem( anon=anon, **kwargs )
 
     def get_zarr_dataset(self, bucket: str, key: str, **kwargs ) -> xr.Dataset:
         consolidated = kwargs.pop( 'consolidated', True )
         return xr.open_zarr( self.s3.get_mapper(f'{bucket}/{key}.zarr' ), consolidated=consolidated, **kwargs )
+
+    @staticmethod
+    def item_path( path: str) -> str:
+        return path.split(":")[-1].replace("//", "/").replace("//", "/")
 
 def eis3( *args, **kwargs ):
     return EIS3.instance( *args, **kwargs )
