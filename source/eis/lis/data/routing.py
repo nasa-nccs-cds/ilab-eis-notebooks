1345e7737a51207526eaaa9c79bf64203af84536
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-06-02 19:14:31 -0400

    Add LIS classes

--- source/eis/lis/data/routing.py
+++ source/eis/lis/data/routing.py
@@ -82,7 +82,7 @@ class LISRoutingData:
         var_stream = Params( var_select, ['value'], rename={'value': 'vname'} )
  #       tindex = param.Integer(default=0, doc='Time Index')
         varmap = self.var_image( streams=[ var_stream ] )
-        point_stream = SingleTap( x=self.x0, y=self.y0, source=varmap ).rename( x='lon', y="lat" )
+        point_stream = SingleTap( x=self.x0, y=self.y0, source=varmap, transient=True ).rename( x='lon', y="lat" )
         vargraph = self.var_graph( streams=[ var_stream, point_stream ] )
         return pn.Row( varmap, pn.Column(  var_select, vargraph ) )
 
