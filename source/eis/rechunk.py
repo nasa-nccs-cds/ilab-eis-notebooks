bb607db582c310731ddd43bc382c5975c4c5fed2
nasa-nccs-cds <thomas.maxwell@nasa.gov>
2021-06-04 17:35:52 -0400

    Add LIS classes

--- source/eis/rechunk.py
+++ source/eis/rechunk.py
@@ -39,10 +39,10 @@ class Rechunker:
         var_chunks = {v: chunks for v in self.dset.data_vars}
         if isinstance( target_store, str ):
             target_store = f"{target_store}/{self.name}.zarr"
-            shutil.rmtree(target_store)
+            shutil.rmtree( target_store, ignore_errors= True )
             print( f"Writing result to {target_store} with max-memory-per-worker set to {max_memory}" )
         if isinstance( temp_store, str):
             temp_store =  f"{temp_store}/{self.name}.zarr"
-            shutil.rmtree(temp_store)
+            shutil.rmtree( temp_store, ignore_errors= True )
             print(f"Using temp_store: {temp_store} with chunks = {var_chunks}")
         return rechunk( self.dset, var_chunks, max_memory,  target_store=target_store, temp_store=temp_store, **kwargs )
